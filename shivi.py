8dc929664fdd06de316768fe817f7e80d10725c7
Nitinmobisec <nitin.kumar@mobisec.in>
2024-12-17 16:43:51 +0530

    key

--- shivi.py
+++ shivi.py
@@ -6,7 +6,10 @@ from openai import OpenAI
 
 # Load API key from .env file
 load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "xai-gWFGh2HowkKLjVb9MWM1XOIMYUpnWonw4iggjmVf6ymPXAic81HrOn53007eTCT7TWeg2KJgLxA06svE"
+
+
 client = OpenAI(
     api_key=openai.api_key ,
     base_url="https://api.x.ai/v1",
